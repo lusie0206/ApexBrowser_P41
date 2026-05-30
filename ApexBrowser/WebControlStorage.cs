5f185463632a685ecba7f9c460f0eebc3198c7f3
lusie0206 <lusie0206@gmail.com>
2026-05-30 17:12:07 +0300

    Implemented "central" storage for IWebControl handling (actual + navigation).

---
+++ ApexBrowser/WebControlStorage.cs
@@ -0,0 +1,43 @@
+﻿using ApexBrowser.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexBrowser;
+
+internal sealed class WebControlStorage
+{
+    #region Singleton
+    public static WebControlStorage Instance { get; } = new();
+
+    private WebControlStorage()
+    {
+    }
+
+    static WebControlStorage()
+    {
+    }
+    #endregion
+
+    public event EventHandler ActiveWebControlChanged;
+    public event EventHandler NotifyWebControlNavigationCompleted;
+
+    private IWebControl webControl;
+
+
+    public IWebControl GetActualWebControl() => webControl;
+
+    public void SetActiveWebControl(IWebControl webControl)
+    {
+        this.webControl = webControl;
+
+        ActiveWebControlChanged?.Invoke(null, EventArgs.Empty);
+    }
+
+    public void NotifyNavigationCompleted()
+    {
+        NotifyWebControlNavigationCompleted?.Invoke(null, EventArgs.Empty);
+    }
+}

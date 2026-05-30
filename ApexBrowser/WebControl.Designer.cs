723d6c53a91e2e0e949155300c11095a56ef6992
lusie0206 <lusie0206@gmail.com>
2026-05-30 15:28:15 +0300

    Added WebControl + base logic for navigation.

---
+++ ApexBrowser/WebControl.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace ApexBrowser
+{
+    partial class WebControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            webView2Control = new Microsoft.Web.WebView2.WinForms.WebView2();
+            labelHeader = new Label();
+            ((System.ComponentModel.ISupportInitialize)webView2Control).BeginInit();
+            SuspendLayout();
+            // 
+            // webView2Control
+            // 
+            webView2Control.AllowExternalDrop = true;
+            webView2Control.CreationProperties = null;
+            webView2Control.DefaultBackgroundColor = Color.White;
+            webView2Control.Location = new Point(0, 36);
+            webView2Control.Name = "webView2Control";
+            webView2Control.Size = new Size(448, 247);
+            webView2Control.TabIndex = 0;
+            webView2Control.ZoomFactor = 1D;
+            // 
+            // labelHeader
+            // 
+            labelHeader.AutoSize = true;
+            labelHeader.Location = new Point(3, 9);
+            labelHeader.Name = "labelHeader";
+            labelHeader.Size = new Size(75, 20);
+            labelHeader.TabIndex = 1;
+            labelHeader.Text = "<header>";
+            // 
+            // WebControl
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(labelHeader);
+            Controls.Add(webView2Control);
+            Name = "WebControl";
+            Size = new Size(454, 286);
+            Load += WebControl_Load;
+            ((System.ComponentModel.ISupportInitialize)webView2Control).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2Control;
+        private Label labelHeader;
+    }
+}
---
+++ ApexBrowser/WebControl.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ApexBrowser
+{
+    public partial class WebControl : UserControl
+    {
+        private const string startupUrl = "https://developer.microsoft.com/en-us/microsoft-edge/webview2/?form=MA13LH";
+
+        public WebControl()
+        {
+            InitializeComponent();
+        }
+
+        private void WebControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InitializeWebView()
+        {
+            if (webView2Control != null)
+            {
+                // 1. Event to get state that Core is loaded
+                webView2Control.CoreWebView2InitializationCompleted += (sender, e) =>
+                {
+                    if (e.IsSuccess)
+                    {
+                        // 2. Event to get state that new page is loaded
+                        webView2Control.CoreWebView2.NavigationCompleted += (sender, e2) =>
+                        {
+                            if (!e.IsSuccess)
+                            {
+                                MessageBox.Show($"Page loading error {e2.WebErrorStatus}");
+                            }
+
+                            //UpdateNavigationButtons();
+                            //UpdateNavigationUrl();
+                        };
+
+                        // Make naviagtion
+                        MakeNavigation(startupUrl);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"WebView2 initialization error: {e.InitializationException.Message}");
+                    }
+                };
+            }
+        }
+
+        private string GetCurrentUri() => webView2Control.Source?.AbsoluteUri ?? string.Empty;
+
+        private void MakeNavigation(string url)
+        {
+            webView2Control.Source = new Uri(url);
+        }
+
+        private void GoBack()
+        {
+            //prev
+            webView2Control.GoBack();
+        }
+
+        private void GoForward()
+        {
+            //next
+            webView2Control.GoForward();
+        }
+
+        private void Reload()
+        {
+            //reload
+            webView2Control.Reload();
+        }
+
+    }
+
+}

b2e7f7597231dce59765b2c7b1182f1f1812af9e
lusie0206 <lusie0206@gmail.com>
2026-05-30 17:11:22 +0300

    Added interface for WebControl.

---
+++ ApexBrowser/Interfaces/IWebControl.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Web.WebView2.WinForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApexBrowser.Interfaces;
+
+public interface IWebControl
+{
+    void GoBack();
+    void GoForward();
+    void Reload();
+    void Navigate(string query);
+    string GetCurrentUrl();
+
+    WebView2 GetWebView2Instance();
+}
